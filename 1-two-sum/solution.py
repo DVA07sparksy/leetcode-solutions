734c51ed58e2d7ec36045ed956d65908ce43e552
DVA07sparksy <dzeufouovictoryatemkutoh@gmail.com>
2026-09-22 23:04:21 +0100

    Two Sum - 2026-09-22T22:04:19.884Z

---
+++ 1-two-sum/solution.py
@@ -0,0 +1,16 @@
+// 2114 ms | 13.4 MB
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        
+        length = len(nums)
+        for i in range(length):
+            for j in range(i+1, length):
+                if (nums[i] + nums[j]) == target:
+                    return [i, j]
+        return []
+    
