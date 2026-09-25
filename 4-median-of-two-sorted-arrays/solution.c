4b4282eb5bb36bfe087698d1241e4ab112ca79ef
DVA07sparksy <dzeufouovictoryatemkutoh@gmail.com>
2026-09-25 20:36:13 +0100

    Median of Two Sorted Arrays - 2026-09-25T19:36:10.877Z

--- 4-median-of-two-sorted-arrays/solution.c
+++ 4-median-of-two-sorted-arrays/solution.c
@@ -1,4 +1,4 @@
-// 83 ms | 12.1 MB
+// 79 ms | 12 MB
 double findMedianSortedArrays(int* nums1, int nums1Size, int* nums2, int nums2Size) {
     int size =nums1Size + nums2Size;
     int temp[size];
@@ -7,10 +7,6 @@ double findMedianSortedArrays(int* nums1, int nums1Size, int* nums2, int nums2Si
     }
     for (int i = 0; i < nums2Size; i++) {
         temp[i + nums1Size] = nums2[i];
-    }
-    double sum=0;
-    for(int i = 0; i < size; i++){
-       sum=sum+temp[i];
     }
     //sort
     for (int i = 0; i < size - 1; i++) {
